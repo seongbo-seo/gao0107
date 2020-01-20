3e84de2cedfe276add8ee72006f98d67be61ccd1
seongbo seo <sungbo0503@hanmail.net>
2020-01-20 11:08:59 +0900

    -add recycler view in favorite fragment -add retrofit in favorite fragment

--- app/src/main/java/com/example/bottomnavigationwithfragment/StoreDetail.java
+++ app/src/main/java/com/example/bottomnavigationwithfragment/StoreDetail.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class StoreDetail extends AppCompatActivity {
 
@@ -20,6 +21,20 @@ public class StoreDetail extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_24px);
 
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+            case R.id.favorite:
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/bottomnavigationwithfragment/fragment/FavoriteFragment.java
+++ app/src/main/java/com/example/bottomnavigationwithfragment/fragment/FavoriteFragment.java
@@ -1,6 +1,8 @@
 package com.example.bottomnavigationwithfragment.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,15 +11,77 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bottomnavigationwithfragment.R;
+import com.example.bottomnavigationwithfragment.ResultRecyclerViewAdapter;
+import com.example.bottomnavigationwithfragment.StoreItem;
+import com.example.bottomnavigationwithfragment.retrofit.RetrofitConnection;
+import com.example.bottomnavigationwithfragment.retrofit.RetrofitInterface;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FavoriteFragment extends Fragment {
     private ViewGroup rootView;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private ArrayList<StoreItem> mArrayList;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = (ViewGroup)inflater.inflate(R.layout.favorit_fragment,container,false);
+
+        //이 부분을 좋아요 목록 테이블을 불러서 구현하면 됨
+        RetrofitConnection retrofitConnection = new RetrofitConnection();
+        RetrofitInterface retrofitInterface = retrofitConnection.retrofit.create(RetrofitInterface.class);
+        retrofitInterface.getData().enqueue(new Callback<JsonArray>() {
+            @Override
+            public void onResponse(Call<JsonArray> call, Response<JsonArray> response) {
+                JsonArray body = response.body();
+                Log.e("response success","success");
+                Log.e("response size",body.size()+"");
+                if(body!=null){
+                    Log.e("response contents",body.toString());
+                    for(int i=0;i<body.size();i++){
+                        JsonObject object= body.get(i).getAsJsonObject();
+                        String storename = object.get("title").getAsString();
+                        String storetime = object.get("completed").getAsString();
+                        String storetoday = object.get("id").getAsString();
+                        StoreItem item = new StoreItem("no",storename,storetime,storetoday);
+                        mArrayList.add(item);
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonArray> call, Throwable t) {
+                Log.e("fail","fail");
+            }
+        });
+
+
+        mArrayList = new ArrayList<>();
+
+        Context context = rootView.getContext();
+        recyclerView = (RecyclerView)rootView.findViewById(R.id.favoritelist_recyclerview);
+        mAdapter = new ResultRecyclerViewAdapter(mArrayList);
+        layoutManager = new LinearLayoutManager(context);
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        recyclerView.setAdapter(mAdapter);
+
         return rootView;
 
     }
